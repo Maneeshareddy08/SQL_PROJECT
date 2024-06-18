a46abec9fe467b11d68adbf59dc3e9675a044dae
Maneeshareddy08 <mundlamaneesha.20@gmail.com>
2024-06-18 16:14:50 -0400

    Query 1

---
+++ project_sql/query1.sql
@@ -0,0 +1,15 @@
+
+SELECT 
+    job_id,
+    job_title,
+    salary_year_avg,
+    name AS company_name 
+FROM job_postings_fact
+LEFT JOIN company_dim AS company ON job_postings_fact.company_id= company.company_id
+WHERE 
+     job_title_short = 'Data Analyst' AND
+     job_location = 'Anywhere' AND
+     salary_year_avg IS NOT NULL
+
+ORDER BY salary_year_avg DESC
+LIMIT 10;
