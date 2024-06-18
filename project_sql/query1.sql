2ad5fe60deffb28a89a8d72e2857668b11cd4e8f
Maneeshareddy08 <mundlamaneesha.20@gmail.com>
2024-06-18 16:32:06 -0400

    updated query

--- project_sql/query1.sql
+++ project_sql/query1.sql
@@ -13,3 +13,19 @@ WHERE
 
 ORDER BY salary_year_avg DESC
 LIMIT 10;
+
+SELECT
+     job_id,
+     job_title,
+     salary_year_avg,
+     job_schedule_type,
+     job_location,
+     name AS company_name
+FROM job_postings_fact
+LEFT JOIN company_dim AS company ON job_postings_fact.company_id = company.company_id
+WHERE 
+    job_title_short = 'Business Analyst' AND
+    job_location = 'New York, NY' AND
+    salary_year_avg IS NOT NULL
+ORDER BY salary_year_avg DESC
+LIMIT 15;
